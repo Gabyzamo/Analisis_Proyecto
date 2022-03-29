6653a48beae19dfe15cdbadce2780f684efd08e6
gabyzamo <zamoraroagaby@gmail.com>
2022-03-29 11:30:49 -0500

    recuperacion2

---
+++ arreglos.js
@@ -0,0 +1,10 @@
+/* es una estructura que almacena cualquier tipo de dato y cualquier cantidad, es decir
+string, int, double, array, objetos, entre otros. los  arreglos o arrays tienen
+operaciones como insertar eliminar, modificar y consultar  
+la declaracion de un arreglo se hace mediante unos corchetes[] // Alt+91 y los elemtos
+dentro de el se separan con comas. Tambien se crea o se declara con las palabras reservadas
+VAR o LET seguidas del nombre de la regla*/
+
+var MiPrimerArreglo=["jose", 14];
+console.log(MiPrimerArreglo);
+var NotaEst=[["sofia", 4.5],["mari",4.2]];
