626133d35a2611ee4fde15178e7ae3306ab2d310
gabyzamo <zamoraroagaby@gmail.com>
2022-04-20 10:20:34 -0500

    trabajo en clase

---
+++ comparacion.js
@@ -0,0 +1,33 @@
+x=5;
+console.log(x>7);//FALSE
+console.log(x<7);//TRUE
+console.log(x==5);//TRUE
+console.log(x==="5");//FALSE
+console.log(x>=1);//TRUE
+console.log(x!=10);//FALSE
+
+
+console.log(2 < 12 );//TRUE
+console.log(2 < "12");// TRUE
+console.log(2 < "John");//FALSE
+console.log(2 > "John");//FALSE
+console.log(2 == "John");//FALSE
+console.log("John" == "John");//TRUE
+console.log("john" == "John");//FALSE
+console.log("2" < "12");//FALSE
+console.log("2" > "12");//TRUE
+console.log("2" == "12");//FALSE
+
+
+x=12;   
+y=7;
+x<15 && y!=7//false
+y>8 && x<20//true
+x==12 && x<18//true
+("lunes" == "Lunes") || (y!=9)//true
+!(x==10) && y>=6//false
+x>6 && y==12 && 2>3//false
+(x*2)>22 && y<(2*4)//false
+(x>10 || x<10) && !(y!=7 && y<21)//true
+((x/2)<=(2*3) || (x*2)>23 ) && (15==="15" && y!=="7")//true
+
